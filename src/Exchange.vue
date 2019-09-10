fa5d63ac2c20fb09239f509d19a7d1025cf9ea30
Modest228 <makskostin1003@yandex.ru>
2019-09-10 18:37:47 +0700

    exchange v5

--- src/Exchange.vue
+++ src/Exchange.vue
@@ -1,22 +1,35 @@
+<template>
  <div class="container">
-            <div class="row about-us-list-wrp">
+            <div class="row about-us-list-wrp style="background-color: #181818">
                 <ul class="about-us-list">
                     <li>
-                        <div class="image-block">
-                            <img src="img/stex.png" alt="Acryl team image">
+                        <div class="item-of-decision image-block">
+                            <img src="img/Logo_stex.svg" alt="Acryl team image">
                         </div>
-                        <div class="text"><strong>{{ $t('exchange.stex') }}</strong>
-                            <!--{{ $t('about.list.acrylTeamText') }}-->
-                        </div>
+                        <!--<div class="text"><strong>{{ $t('exchange.stex') }}</strong>-->
+                            <!--&lt;!&ndash;{{ $t('about.list.acrylTeamText') }}&ndash;&gt;-->
+                        <!--</div>-->
                     </li>
                     <li>
-                        <div class="image-block">
-                            <img src="img/coingecko.jpg" alt="Acryl team image">
+                        <div class="item-of-decision image-block">
+                            <img src="img/Logo-coingecko.webp" alt="Acryl team image">
                         </div>
-                        <div class="text"><strong>{{ $t('exchange.coingecko') }}</strong>
-                            <!--{{ $t('about.list.acrylTeamText') }}-->
-                        </div>
+                        <!--<div class="text"><strong>{{ $t('exchange.coingecko') }}</strong>-->
+                            <!--&lt;!&ndash;{{ $t('about.list.acrylTeamText') }}&ndash;&gt;-->
+                        <!--</div>-->
                     </li>
                 </ul>
             </div>
         </div>
+</template>
+
+<script>
+    import locale from '../src/locale.js'
+    
+    export default{
+        i18n: {
+            messages: {},
+            sharedMessages: locale
+        }
+    }
+</script>
